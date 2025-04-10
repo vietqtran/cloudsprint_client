4270d1d7417fe255923f0261d8fa66dffd0aadee
vietqtran <tranquocviet1303@gmail.com>
2025-04-10 15:15:00 +0700

    fix: confirm password type

--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -223,7 +223,7 @@ export function SignUpForm() {
                 <FormControl>
                   <Input
                     id='confirmPassword'
-                    type='confirmPassword'
+                    type='password'
                     placeholder='••••••••'
                     className={`w-full rounded-md border px-3 py-2`}
                     isError={!!form.formState.errors.confirmPassword}
