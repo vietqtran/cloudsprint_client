b0ec6310e8b6426be803ba26dc43e65599d1dd9a
vietqtran <tranquocviet1303@gmail.com>
2025-04-13 20:17:25 +0700

    refactor: change to yarn 1.22.22

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,8 +1,21 @@
+import { Header } from '@/components/common/layout/Header';
+import { AppSidebar } from '@/components/common/layout/Sidebar';
+import { SidebarProvider } from '@/components/ui/sidebar';
 import AuthProvider from '@/providers/AuthProvider';
 import React from 'react';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <AuthProvider>
+        <div className='size-full'>
+          <Header />
+          {children}
+        </div>
+      </AuthProvider>
+    </SidebarProvider>
+  );
 };
 
 export default RootLayout;
